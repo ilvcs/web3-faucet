9c9fb3ac6db21471e2e1f52b710c76b92d1dda2a
ilvcs <rammbbs2008@gmail.com>
2023-10-14 23:32:00 +0530

    Blank repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+function App() {
+	return (
+		<VStack w='full' h='100vh'>
+			<Heading>Hello world</Heading>
+		</VStack>
+	);
+}
+
+export default App;
